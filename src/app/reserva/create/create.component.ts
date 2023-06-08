ddf79521024cc164e290e4a4e7f048be1848cecf
elbarrun <dam10.2022.jesuitas@gmail.com>
2023-06-08 14:02:50 +0200

    ue

--- src/app/reserva/create/create.component.ts
+++ src/app/reserva/create/create.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ReservaService } from '../reserva.service';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, NgForm, FormBuilder} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 import { throwError, Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -17,24 +17,13 @@ export class CreateComponent implements OnInit {
   selectedImage: any;
   categorias:any = [];
 
-  /*------------------------------------------
-  --------------------------------------------
-  Created constructor
-  --------------------------------------------
-  --------------------------------------------*/
   constructor(
     public reservaService: ReservaService,
     private router: Router,
     private http: HttpClient,
     public fb: FormBuilder,
   ) { }
-}
 
-  /**
-   * Write code on Method
-   *
-   * @return response()
-   */
   ngOnInit() {
     this.form = new FormGroup({
       user_id: new FormControl(null, Validators.required),
@@ -44,23 +33,11 @@ export class CreateComponent implements OnInit {
       precio_total: new FormControl(null, Validators.required),
       estado: new FormControl(null, Validators.required)
     });
-
   }
 
-  /**
-   * Write code on Method
-   *
-   * @return response()
-   */
   get f(){
     return this.form.controls;
   }
-
-  /**
-   * Write code on Method
-   *
-   * @return response()
-   */
 
   uploadFile(event: any) {
     const file = (event.target as HTMLInputElement)?.files?.[0];
@@ -68,15 +45,16 @@ export class CreateComponent implements OnInit {
       file: file
     });
   }
+
   submit(){
-    const reserva = {
-      user_id: this.form.value.user_id,
-      coche_id: this.form.value.coche_id,
-      fecha_inicio: this.form.value.fecha_inicio,
-      fecha_fin: this.form.value.fecha_fin,
-      precio_total: this.form.value.precio_total,
-      estado: this.form.value.estado
-    };
+    const reserva = new FormData();
+    reserva.append('user_id', this.form.value.user_id);
+    reserva.append('coche_id', this.form.value.coche_id);
+    reserva.append('fecha_inicio', this.form.value.fecha_inicio);
+    reserva.append('fecha_fin', this.form.value.fecha_fin);
+    reserva.append('precio_total', this.form.value.precio_total);
+    reserva.append('estado', this.form.value.estado);
+
 
     this.reservaService.create(reserva).subscribe(
       (response:any) => {
@@ -88,4 +66,4 @@ export class CreateComponent implements OnInit {
       }
     );
   }
-
+}
